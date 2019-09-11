82fdd542a801bfe6482d3c3f9dd7b32389b56bfa
zloyleva <zloyleva.imt@gmail.com>
2019-09-11 19:43:08 +0300

    add ProductCard

---
+++ src/components/Header/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item href="#">Link</b-nav-item>
+                <b-nav-item href="#">Disabled</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ProductCard/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <b-col md="6">
+        <h1 class="text-capitalize border-bottom">product1</h1>
+        <div class="my-4"><img src="http://smktesting.herokuapp.com/static/img1.png" class="rounded">
+        </div>
+        <div class="border-top"><h3 class="h4">Product Description</h3>
+            <p>lorem ipsum 1</p>
+        </div>
+    </b-col>
+</template>
+
+<script>
+    export default {
+        name: "ProductCard"
+    }
+</script>
+
+<style scoped>
+
+</style>
